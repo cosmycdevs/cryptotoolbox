4d7e1ce61ea8dc19a7b94b0ac4b557c80161d7d4
Stivius <maxim11853@gmail.com>
2017-11-14 18:14:23 +0200

    Vanity address test

---
+++ VanityAddressTest/VanityAddressTest.pro
@@ -0,0 +1,25 @@
+QT -= gui
+
+CONFIG += console
+CONFIG -= app_bundle c++14
+
+# The following define makes your compiler emit warnings if you use
+# any feature of Qt which as been marked deprecated (the exact warnings
+# depend on your compiler). Please consult the documentation of the
+# deprecated API in order to know how to port your code away from it.
+DEFINES += QT_DEPRECATED_WARNINGS
+
+# You can also make your code fail to compile if you use deprecated APIs.
+# In order to do so, uncomment the following line.
+# You can also select to disable deprecated APIs only up to a certain version of Qt.
+#DEFINES += QT_DISABLE_DEPRECATED_BEFORE=0x060000    # disables all the APIs deprecated before Qt 6.0.0
+
+INCLUDEPATH += /home/stivius/boost_1_65_1
+
+QMAKE_CXXFLAGS += -std=c++14
+
+SOURCES += main.cpp \
+    base58.cpp
+
+HEADERS += \
+    base58.h
---
+++ VanityAddressTest/main.cpp
@@ -0,0 +1,79 @@
+#include <iostream>
+#include <limits>
+#include <chrono>
+#include <sstream>
+#include <boost/multiprecision/cpp_int.hpp>
+
+#include "base58.h"
+
+using namespace boost::multiprecision;
+
+// base58
+const uint256_t Base = 58;
+
+// maximum 24-byte number
+const uint256_t Max24ByteNumber("6277101735386680763835789423207666416102355444464034512895");
+
+// minimum 24-byte number
+const uint256_t Min24ByteNumber("24519928653854221733733552434404946937899825954937634816");
+
+// 2^192 / 2^160 (we remove 4-byte checksum)
+const boost::uint64_t ChecksumBytes = 4294967296;
+
+// 2^160 (all possible variant of 20-byte number)
+const uint256_t MaxAmountOf20byteNumbers("1461501637330902918203684832716283019655932542976");
+
+uint256_t GetDecimalPattern(const std::string& stringPattern)
+{
+    uint256_t pattern;
+    std::stringstream ss;
+    std::vector<unsigned char> res;
+    DecodeBase58(stringPattern.c_str(), res);
+    for(auto&& c : res)
+    {
+        ss << std::hex << static_cast<short>(c);
+    }
+    ss >> pattern;
+    return pattern;
+}
+
+auto FindRanges(uint256_t pattern)
+{
+    std::vector<std::pair<uint256_t, uint256_t>> ranges;
+    uint256_t beginRange = 0, endRange = 0;
+    uint16_t extent = 0;
+
+    while(beginRange <= Max24ByteNumber && endRange <= Max24ByteNumber) // until we reach 25-byte number
+    {
+        beginRange = pattern * boost::multiprecision::pow(Base, extent);
+        endRange = (pattern + 1) * boost::multiprecision::pow(Base, extent) - 1;
+
+        if(beginRange >= Min24ByteNumber && beginRange <= Max24ByteNumber &&
+           endRange >= Min24ByteNumber && endRange <= Max24ByteNumber)
+        {
+            ranges.emplace_back(std::make_pair(beginRange, endRange));
+        }
+        ++extent;
+    }
+
+    return ranges;
+}
+
+int main()
+{
+    auto begin = std::chrono::system_clock::now();
+    auto ranges = FindRanges(GetDecimalPattern("1A"));
+
+    uint256_t result;
+    // summing all variants that match our pattern
+    for(auto&& pair : ranges)
+    {
+        result += pair.second - pair.first;
+    }
+
+    auto end = std::chrono::system_clock::now();
+
+    std::cout << "Complexity: " << MaxAmountOf20byteNumbers/(result/ChecksumBytes) << std::endl;
+    std::cout << "Duration (microseconds): " << std::chrono::duration<double, std::micro>(end - begin).count() << std::endl;
+    return 0;
+}
